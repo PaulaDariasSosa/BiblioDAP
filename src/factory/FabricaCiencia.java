5235bef76e5d81812cbb9085275f85243b11d200
mikelmujika16 <124912823+mikelmujika16@users.noreply.github.com>
2024-11-12 23:22:55 +0000

    CienciaPrimerPublicacion

--- src/factory/FabricaCiencia.java
+++ src/factory/FabricaCiencia.java
@@ -1,7 +1,9 @@
 package factory;
 
 import model.Libro;
+import model.LibroCiencia;
 import model.Revista;
+import model.RevistaCiencia;
 import service.ConexionAPILibros;
 import service.ConexionAPIRevistas;
 
@@ -13,17 +15,18 @@ public class FabricaCiencia implements Fabrica {
     }
 
     @Override
-    public Revista crearRevista() {
+    public RevistaCiencia crearRevista() {
         ConexionAPIRevistas conexion = new ConexionAPIRevistas("https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:%22science%22&sort=newest&api-key=BsEGWJx6KO6rf0Y9OJJh692KPvCUUAUC&fl=abstract,lead_paragraph,source,web_url,word_count");
         ArrayList<ArrayList<String>> datos_ = conexion.getInformacion().getDatos();
-        System.out.println(datos_.toString());
-        return null;
+        RevistaCiencia revista_ = new RevistaCiencia(datos_.get(0).get(0), datos_.get(0).get(1), datos_.get(0).get(2), datos_.get(0).get(3), Integer.parseInt(datos_.get(0).get(4)));
+        return revista_;
     }
 
     @Override
-    public Libro crearLibro() {
+    public LibroCiencia crearLibro() {
         ConexionAPILibros conexion = new ConexionAPILibros("https://openlibrary.org/search.json?page=1&sorts=already_read_count%20desc&fields=title,publish_date,isbn,author_name&subject=science&limit=10");
         ArrayList<ArrayList<String>> datos_ = conexion.getInformacion().getDatos();
-        return new Libro(datos_.get(0).get(0), datos_.get(0).get(3), Integer.parseInt(datos_.get(0).get(2)), datos_.get(0).get(1));
+        LibroCiencia libro_ = new LibroCiencia(datos_.get(0).get(0), datos_.get(0).get(3), Integer.parseInt(datos_.get(0).get(2)), datos_.get(0).get(1));
+        return libro_;
     }
 }
---
+++ src/model/LibroCiencia.java
@@ -0,0 +1,22 @@
+package model;
+
+public class LibroCiencia extends Libro {
+
+    public LibroCiencia() {
+        super();
+    }
+
+    public LibroCiencia(String titulo, String autor, int isbn, String fecha) {
+        super(titulo, autor, isbn, fecha);
+    }
+
+    public void mostrar() {
+        System.out.println("Titulo: " + titulo_);
+        System.out.println("Autor: " + autor_);
+        System.out.println("ISBN: " + isbn_);
+        System.out.println("Fecha: " + fecha_);
+        System.out.println("Es un libro de Ciencia");
+    }
+
+
+}
