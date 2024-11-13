e33a5dbd3180570f4229df8eef5b1d049440bb91
Laura González <72458436+alu0101203942@users.noreply.github.com>
2024-11-13 23:05:46 +0000

    Add files via upload

---
+++ src/model/LibroVideo.java
@@ -0,0 +1,43 @@
+package model;
+
+import service.*;
+import java.util.Scanner;
+
+public class LibroVideo extends Libro {
+    private long isbn_;
+
+    public LibroVideo() {
+        super();
+        ConexionAPILibros conexion = new ConexionAPILibros("https://openlibrary.org/search.json?page=1&sorts=already_read_count%20desc&fields=title,publish_date,isbn,author_name&subject=video&limit=10");
+        Dataset informacion = conexion.getInformacion();
+
+        mostrarLibros(informacion);
+        int opcion = obtenerOpcion(informacion);
+
+        asignarDatos(informacion, opcion);
+    }
+
+    private void mostrarLibros(Dataset informacion) {
+        System.out.println("Libros de Video:");
+        informacion.show();
+    }
+
+    private int obtenerOpcion(Dataset informacion) {
+        System.out.println("Elija un libro de Video (introduzca un numero de 1 a " + informacion.getDatos().size() + "):");
+        Scanner scanner = new Scanner(System.in);
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
+        return opcion;
+    }
+
+    private void asignarDatos(Dataset informacion, int opcion) {
+        titulo_ = informacion.getDatos().get(opcion - 1).get(3);
+        autor_ = informacion.getDatos().get(opcion - 1).get(0);
+        isbn_ = Long.parseLong(informacion.getDatos().get(opcion - 1).get(1));
+        fecha_ = informacion.getDatos().get(opcion - 1).get(2);
+    }
+
+    public String toString() {
+        return "Titulo: " + titulo_ + "\n" + "Autor: " + autor_ + "\n" + "ISBN: " + isbn_ + "\n" + "Fecha: " + fecha_ + "\n" + "Es un libro de Viajes";
+    }
+}
