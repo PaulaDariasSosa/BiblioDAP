351e2ebd8d92de993698f299346613e17ffed1f7
Paula Darias Sosa <pmdsosa@gmail.com>
2024-11-14 00:39:59 +0000

    Creado diagrama

--- src/factory/FabricaCiencia.java
+++ src/factory/FabricaCiencia.java
@@ -21,7 +21,7 @@ public class FabricaCiencia implements Fabrica {
      * @return
      */
     @Override
-    public RevistaCiencia crearRevista() {
+    public Revista crearRevista() {
         ConexionAPIRevistas conexion = new ConexionAPIRevistas("https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:%22science%22&sort=newest&api-key=BsEGWJx6KO6rf0Y9OJJh692KPvCUUAUC&fl=abstract,lead_paragraph,source,web_url,word_count");
         Dataset informacion = conexion.getInformacion();
         ArrayList<ArrayList<String>> datos_ = conexion.getInformacion().getDatos();
@@ -41,7 +41,7 @@ public class FabricaCiencia implements Fabrica {
      * @return
      */
     @Override
-    public LibroCiencia crearLibro() {
+    public Libro crearLibro() {
         ConexionAPILibros conexion = new ConexionAPILibros("https://openlibrary.org/search.json?page=1&sorts=already_read_count%20desc&fields=title,publish_date,isbn,author_name&subject=science&limit=10");
         Dataset informacion = conexion.getInformacion();
         ArrayList<ArrayList<String>> datos_ = informacion.getDatos();
